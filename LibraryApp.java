d3f137185bda14cdc8e8e36862844399bb1df187
Rohit Ambhure <125722330+Rohit16556@users.noreply.github.com>
2026-09-11 01:10:03 +0530

    Update LibraryApp.java

--- LibraryApp.java
+++ LibraryApp.java
@@ -5,20 +5,20 @@ public class LibraryApp {
 
 	Scanner sc = new Scanner(System.in);
 	
-    	final String admin_name = "admin1";
+    final String admin_name = "admin1";
    	final String admin_pass = "admin@123";
 
   	ArrayList<Book> books = new ArrayList<>();
-    	ArrayList<Student> students = new ArrayList<>();
-    	ArrayList<String[]> borrowedBooks = new ArrayList<>();
+    ArrayList<Student> students = new ArrayList<>();
+    ArrayList<String[]> borrowedBooks = new ArrayList<>();
 
 	public static void main(String[] args) {
-	LibraryApp lab = new LibraryApp();
-	lab.start();
+		LibraryApp lab = new LibraryApp();
+		lab.start();
 	}
 
 private void clearScreen() {
-    try {
+	try {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
     } catch (Exception e) {
         e.printStackTrace();
@@ -62,8 +62,10 @@ public void start() {
 }
 
 private void adminLogin() {
-	clearScreen();
+		clearScreen();
+	
         System.out.println("Admin Login..");
+	
         System.out.print("Username: ");
         String username = sc.nextLine();
 
@@ -72,22 +74,28 @@ private void adminLogin() {
 
         if (username.equals(admin_name) && password.equals(admin_pass)){
 
-            System.out.println("Admin login successfully.");        
+            System.out.println("Admin login successfully."); 
+			
             adminMenu();
-	    return;
+			
+	    	return;
 
-        } else {
-		System.out.println("Invalid Username or Password.");
-		start();
+    	} else {
+			System.out.println("Invalid Username or Password.");
+			start();
+		}
 	}
-    }
 
 private void adminMenu() {
+	
         clearScreen();
-	int ch;
+	
+		int ch;
         do {
+			
             System.out.println("\n ** Admin menu **");
-            System.out.println("\n1.Register a new book");
+            
+			System.out.println("\n1.Register a new book");
             System.out.println("2.Update book quantity");
             System.out.println("3.View books taken by students");
             System.out.println("4.View all books");
@@ -95,10 +103,10 @@ private void adminMenu() {
 
             System.out.print("\nEnter choice: ");
 
-            ch=sc.nextInt();
+            ch = sc.nextInt();
             sc.nextLine();
 
-            switch(ch){
+            switch(ch) {
                 case 1:
                 	registerBook();
                     	break;
@@ -114,49 +122,59 @@ private void adminMenu() {
                 case 5:
                         System.out.println("Log out.");
                         start();
-			return;
+						return;
                   
                 default:
                     System.out.println("Invalid choice.");
             }
-
+			
         } while (ch != 5);
-    }
+	}
 
 private void registerBook() {
-    clearScreen();
-    System.out.println("Register new book..");
-    System.out.print("Book name: ");
-    String name =sc.nextLine();
+		clearScreen();
+    	
+		System.out.println("Register new book..");
+    	
+		System.out.print("Book name: ");
+    	String name =sc.nextLine();
 
-    System.out.print("Author: ");
-    String author =sc.nextLine();
+    	System.out.print("Author: ");
+    	String author =sc.nextLine();
 
-    System.out.print("Quantity: ");
-    int quantity =sc.nextInt();
-    sc.nextLine();
+    	System.out.print("Quantity: ");
+    	int quantity =sc.nextInt();
+    	sc.nextLine();
 
-    Book book = new Book(name, author, quantity);
-    books.add(book);
-    System.out.println("Book registered successfully.");
+    	Book book = new Book(name, author, quantity);
+    
+		books.add(book);
+    	
+		System.out.println("Book registered successfully.");
     }
 
-private void viewAllBooks(){
+private void viewAllBooks() {
 
-     if(books.isEmpty()) {
+    if(books.isEmpty()) { 	 
         System.out.println("No books available.");
    	}
+	
     System.out.println("\nAvailable books in library..");
 
     for(int i=0; i<books.size(); i++) {
-        Book book =books.get(i);
-        System.out.println((i + 1) + ".  " + book);
+       
+		Book book =books.get(i);
+        
+		System.out.println((i + 1) + ".  " + book);
     }
 }
 
 private Student findStudent(String name) {
+	
 	for(Student student : students) {
+	
 		if(student.username.equals(name)) {
+		
 			return student;
 		}
 	}
@@ -164,12 +182,12 @@ private Student findStudent(String name) {
 }
 
 private void updateQuantity() {
-    System.out.println("Update Books Quantity.");
+	System.out.println("Update Books Quantity.");
         
 	System.out.println("Enter book name: ");
-    	String name = sc.nextLine();
+    String name = sc.nextLine();
 
-    	Book book = findBook(name);
+    Book book = findBook(name);
 
 	if(book == null) {
 		System.out.println("book not found.");
@@ -194,9 +212,11 @@ private void updateQuantity() {
 }
 
 private Book findBook(String name) {
-    for(Book book:books) {
-        if(book.name.equalsIgnoreCase(name)) {
-            return book;
+ 
+	for(Book book:books) {
+    
+		if(book.name.equalsIgnoreCase(name)) {
+			return book;
         }
     }
     return null;
@@ -218,14 +238,17 @@ private void studentRegister() {
     String password = sc.nextLine();
 
     Student student = new Student(username, password);
-    students.add(student);
-    System.out.println("Registration successful.");
+   
+	students.add(student);
+    
+	System.out.println("Registration successful.");
     studentLogin();
 }
 
 private void studentLogin() {
     clearScreen();
-    System.out.println("\nCandidate login: ");
+   
+	System.out.println("\nCandidate login: ");
 
     System.out.print("Username: ");
     String username = sc.nextLine();
@@ -236,28 +259,34 @@ private void studentLogin() {
     Student student = findStudent(username);
 
     if(student == null) {
-        System.out.println("Username not found.");
-        studentRegister();
-        return;
+        
+		System.out.println("Username not found.");
+        
+		studentRegister();
+        
+		return;
     }
 
     if(!student.password.equals(password)) {
     	System.out.println("password does not match.");
-    	studentRegister();
+    	
+		studentRegister();
         return;
     }
 
     System.out.println("Login successfully.");
 
     studentMenu(student);
-    }
+}
 
 private void studentMenu(Student student){
-    int choice;
+   
+	int choice;
     do {
        
         System.out.println("\nWelcome to A Library ");
-        System.out.println("\n1.view books");
+        
+		System.out.println("\n1.view books");
         System.out.println("2.Borrow a book");
         System.out.println("3.Return a book");
         System.out.println("4.Logout");
@@ -295,8 +324,9 @@ private void studentMenu(Student student){
 private void viewBooks() {
 
     if(books.isEmpty()) {
-        System.out.println("There are no books available.");
-	return;
+        
+		System.out.println("There are no books available.");
+		return;
     }
 
 	System.out.println("\nAvailable books");
@@ -310,20 +340,24 @@ private void viewBooks() {
         System.out.println();
 
         for(int i = start; i < end; i++) {
-            Book book = books.get(i);
-            System.out.println((i + 1) + ". " + book);
+            
+			Book book = books.get(i);
+            
+			System.out.println((i + 1) + ". " + book);
         }
 
         if(end >= books.size()) {
-            System.out.println("\nEnd of page");
+           
+			System.out.println("\nEnd of page");
             break;
         }
 
 	System.out.print("press yes to see more, or type x to stop: ");
+		
 	String input=sc.nextLine();
-
+	
 	if(input.equals("x")) {
-	break;
+		break;
 	}
 
 	start=end;
@@ -333,8 +367,7 @@ private void viewBooks() {
 
 private void borrowBook(Student student) {
 
-    System.out.println();
-    System.out.println("borrow book: ");
+    System.out.println("\nborrow book: ");
 
     System.out.print("Enter book name: ");
     String name = sc.nextLine();
@@ -342,45 +375,57 @@ private void borrowBook(Student student) {
     Book book=findBook(name);
 
     if(student.borrowedBooks.contains(book.name)) {
-        System.out.println("You already borrowed this book.");
-        return;
+       
+		System.out.println("You already borrowed this book.");
+        
+		return;
     }
 
     if(book==null){
-        System.out.println("Book not found.");
-        return;
+        
+		System.out.println("Book not found.");
+        
+		return;
     }
 
     if(book.available <=0) {
-        System.out.println("Book is not available.");
-        return;
+        
+		System.out.println("Book is not available.");
+        
+		return;
     }
    
     book.available--;
 
     student.borrowedBooks.add(book.name);
-    borrowedBooks.add(new String[]{student.username, book.name});
-    System.out.println("book borrowed sucessfully.");
+   
+	borrowedBooks.add(new String[]{student.username, book.name});
+    
+	System.out.println("book borrowed sucessfully.");
 }
 
 private void returnBook(Student student) {
 
     System.out.println();
-    System.out.println("return book");
+    System.out.println("Return book");
 
     System.out.print("Enter book name: ");
-    String name =sc.nextLine();
+    String name = sc.nextLine();
 
-    Book book =findBook(name);
+    Book book = findBook(name);
 
-    if(book ==null){
-        System.out.println("Book not found.");
-        return;
+    if(book == null){
+        
+		System.out.println("Book not found.");
+        
+		return;
     }
 
     if(!(student.borrowedBooks.contains(book.name))) {
-        System.out.println("You have not borrowed this book.");
-        return;
+       
+		System.out.println("You have not borrowed this book.");
+        
+		return;
     }
 
     book.available++;
@@ -388,28 +433,33 @@ private void returnBook(Student student) {
     student.borrowedBooks.remove(book.name);
 
     for(int i=0; i< borrowedBooks.size(); i++) {
-	String [] record = borrowedBooks.get(i);
 	
-	if(record[0].equals(student.username) && record[1].equals(book.name)) {
-		borrowedBooks.remove(i);
-		break;
+		String [] record = borrowedBooks.get(i);
+	
+		if(record[0].equals(student.username) && record[1].equals(book.name)) {
+			
+			borrowedBooks.remove(i);
+			break;
 		}
 	}
 	
     System.out.println("Book submit back to the lab successfully...");
 }
 
-
 private void borrowedBooks() {
 	
 	if(borrowedBooks.isEmpty()){
+		
 		System.out.println("No one has borrowed any book. ");
 		return;
+	}
 	
-}
 	System.out.println("Student Name:         Borrowed Books: ");
+	
 	for(int i=0; i<borrowedBooks.size(); i++) {
+		
 		String[] records = borrowedBooks.get(i);
+	
 		System.out.println(records[0] + "                  " + records[1]);
 		}
 	}
